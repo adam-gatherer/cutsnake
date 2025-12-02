18fb09778e6ebbef134fb99b3f82828a1bb4f424
Adam <adam@gatherer.tech>
2025-12-02 16:35:20 +0000

    Created db module

--- infra/terraform/envs/dev/main.tf
+++ infra/terraform/envs/dev/main.tf
@@ -1,3 +1,10 @@
 locals {
   name_prefix = "${var.project}-${var.env}"
 }
+
+module "db" {
+  source     = "../../modules/db"
+  project    = var.project
+  env        = var.env
+  aws_region = var.aws_region
+}
---
+++ infra/terraform/modules/db/main.tf
@@ -0,0 +1,64 @@
+# module specific var
+locals {
+  name_prefix = "${var.project}-${var.env}"
+}
+
+# create dynamodb table
+resource "aws_dynamodb_table" "links" {
+  name         = "${locals.name_prefix}-links"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "pk"
+  range_key    = "sk"
+
+  attribute {
+    name = "pk"
+    type = "S"
+  }
+
+  attribute {
+    name = "sk"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "ttl"
+    enabled        = true
+  }
+
+  tags = {
+    Project = var.project
+    Env     = var.env
+  }
+}
+
+
+# analytics table
+resource "aws_dynamodb_table" "analytics" {
+  name         = "${locals.name_prefix}-analytics"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "pk"
+  range_key    = "sk"
+
+  attribute {
+    name = "pk"
+    type = "S"
+  }
+
+  attribute {
+    name = "sk"
+    type = "S"
+  }
+
+  tags = {
+    Project = var.project
+    Env     = var.env
+  }
+}
+
+output "links_table_name" {
+  value = aws_dynamodb_table.links.name
+}
+
+output "analytics_table_name" {
+  value = aws_dynamodb_table.analytics.name
+}
